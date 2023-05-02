df9275d7dadf3535a87e4c904f1325a3924f3068
Padma Ravi <star.u.like@gmail.com>
2023-05-02 13:22:15 +0530

    kube-project-connection-b/w to mangoservice to mangodb

--- k8-roboshop-project/catalogue/server.js
+++ k8-roboshop-project/catalogue/server.js
@@ -65,7 +65,7 @@ app.get('/health', (req, res) => {
 
 // all products
 app.get('/products', (req, res) => {
-    if(mongoConnected) {
+    if (mongoConnected) {
         collection.find({}).toArray().then((products) => {
             res.json(products);
         }).catch((e) => {
@@ -80,21 +80,21 @@ app.get('/products', (req, res) => {
 
 // product by SKU
 app.get('/product/:sku', (req, res) => {
-    if(mongoConnected) {
+    if (mongoConnected) {
         // optionally slow this down
         const delay = process.env.GO_SLOW || 0;
         setTimeout(() => {
-        collection.findOne({sku: req.params.sku}).then((product) => {
-            req.log.info('product', product);
-            if(product) {
-                res.json(product);
-            } else {
-                res.status(404).send('SKU not found');
-            }
-        }).catch((e) => {
-            req.log.error('ERROR', e);
-            res.status(500).send(e);
-        });
+            collection.findOne({ sku: req.params.sku }).then((product) => {
+                req.log.info('product', product);
+                if (product) {
+                    res.json(product);
+                } else {
+                    res.status(404).send('SKU not found');
+                }
+            }).catch((e) => {
+                req.log.error('ERROR', e);
+                res.status(500).send(e);
+            });
         }, delay);
     } else {
         req.log.error('database not available');
@@ -104,9 +104,9 @@ app.get('/product/:sku', (req, res) => {
 
 // products in a category
 app.get('/products/:cat', (req, res) => {
-    if(mongoConnected) {
+    if (mongoConnected) {
         collection.find({ categories: req.params.cat }).sort({ name: 1 }).toArray().then((products) => {
-            if(products) {
+            if (products) {
                 res.json(products);
             } else {
                 res.status(404).send('No products for ' + req.params.cat);
@@ -123,7 +123,7 @@ app.get('/products/:cat', (req, res) => {
 
 // all categories
 app.get('/categories', (req, res) => {
-    if(mongoConnected) {
+    if (mongoConnected) {
         collection.distinct('categories').then((categories) => {
             res.json(categories);
         }).catch((e) => {
@@ -138,8 +138,8 @@ app.get('/categories', (req, res) => {
 
 // search name and description
 app.get('/search/:text', (req, res) => {
-    if(mongoConnected) {
-        collection.find({ '$text': { '$search': req.params.text }}).toArray().then((hits) => {
+    if (mongoConnected) {
+        collection.find({ '$text': { '$search': req.params.text } }).toArray().then((hits) => {
             res.json(hits);
         }).catch((e) => {
             req.log.error('ERROR', e);
@@ -154,9 +154,9 @@ app.get('/search/:text', (req, res) => {
 // set up Mongo
 function mongoConnect() {
     return new Promise((resolve, reject) => {
-        var mongoURL = process.env.MONGO_URL || 'mongodb://mongodb:27017/catalogue';
+        var mongoURL = process.env.MONGO_URL || 'mongodb://mongodb-service:27017/catalogue';
         mongoClient.connect(mongoURL, (error, client) => {
-            if(error) {
+            if (error) {
                 reject(error);
             } else {
                 db = client.db('catalogue');
